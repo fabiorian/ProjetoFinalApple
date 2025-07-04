fe1400cc9c1079e32ce1a9f3e5037fff801e231a
Fábio <fabiorian718@gmail.com>
2025-07-04 16:48:07 -0300

    Initial Commit

---
+++ DesApega/DesApegaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesApegaApp.swift
+//  DesApega
+//
+//  Created by found on 04/07/25.
+//
+
+import SwiftUI
+
+@main
+struct DesApegaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
