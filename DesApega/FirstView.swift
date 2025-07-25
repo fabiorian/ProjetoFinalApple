56bd78e351f6e3acb534d7f305d54aaea9da793a
Dylan Mesquita <dylanmmelo@gmail.com>
2025-07-25 13:40:38 -0300

    erros de push

---
+++ DesApega/FirstView.swift
@@ -0,0 +1,19 @@
+//
+//  FirstView.swift
+//  DesApega
+//
+//  Created by found on 04/07/25.
+//
+
+import SwiftUI
+
+struct FirstView: View {
+    var body: some View {
+        VStack {
+            Text("First View")
+        }
+    }
+}
+#Preview {
+    FirstView()
+}
