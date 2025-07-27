798a72fe956aa169b77d12a2d9615ae27461243a
Enzo Rodrigues <enzo.rodrigues62@aluno.ifce.edu.br>
2025-07-27 14:12:45 -0300

    Unificação das telas parcial

--- DesApega/DesApegaApp.swift
+++ DesApega/DesApegaApp.swift
@@ -8,10 +8,11 @@
 import SwiftUI
 
 @main
-struct DesApegaApp: App {
+struct TeladeboasvindasApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: [Donation.self])
     }
 }
---
+++ DesApega/cadastro&historico/Donation.swift
@@ -0,0 +1,58 @@
+//
+//  Donation.swift
+//  DesApega
+//
+//  Created by found on 27/07/25.
+//
+
+import Foundation
+import SwiftData
+
+enum Status: String, Codable, CaseIterable {
+    case muito_ruim = "Muito ruim"
+    case ruim = "Ruim"
+    case bom = "Bom"
+    case muito_bom = "Muito bom"
+    case otimo = "Ótimo"
+}
+
+enum Category: String, Codable, CaseIterable {
+    case clothe = "Roupa"
+    case footwear = "Calçado"
+    case eletronic = "Eletrônico"
+}
+
+@Model
+class Donation {
+    var id: UUID
+    var name: String
+    var status: Status
+    var category: Category
+    var detalhes: String
+    var doador: String
+    var local: String
+    var time: Date
+    var imageData: Data?
+
+    init(
+        id: UUID = UUID(),
+        name: String,
+        status: Status,
+        category: Category,
+        detalhes: String,
+        doador: String,
+        local: String,
+        time: Date = Date(),
+        imageData: Data? = nil
+    ) {
+        self.id = id
+        self.name = name
+        self.status = status
+        self.category = category
+        self.detalhes = detalhes
+        self.doador = doador
+        self.local = local
+        self.time = time
+        self.imageData = imageData
+    }
+}
